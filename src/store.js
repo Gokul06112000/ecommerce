c1ba324c1a5ae4f66f18f963013da5e3d6254c4e
Gokul <ggk63358@gmail.com>
2024-03-16 17:30:06 +0530

    third commit

--- src/store.js
+++ src/store.js
@@ -1,14 +1,15 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import thunk  from "redux-thunk";
+import {combineReducers,configureStore} from "@reduxjs/toolkit";
+import productsReducer from "./slices/productsSlice";
+import productReducer from "./slices/productSlice"
+
 
 const reducer = combineReducers({
-
+     productsState: productsReducer,
+     productState: productReducer
+     
 })
-
-
 const store = configureStore({
     reducer,
-    middleware:[thunk]
+  
 })
-
 export default store;
